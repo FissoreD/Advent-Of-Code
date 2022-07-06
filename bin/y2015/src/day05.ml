bdbb6b34466b0c78cc5b3f9a9f1317470ca592a9
FissoreD <davide.fissore@etu.univ-cotedazur.fr>
2022-07-06 22:04:46 +0200

    Add day 12

--- bin/y2015/src/day05.ml
+++ bin/y2015/src/day05.ml
@@ -5,9 +5,9 @@ module P1 = struct
     let vowels = "aeiou" in
     let f a c = a + if String.contains vowels c then 1 else 0 in
     String.fold_left f 0 s > 2
-    && Str.string_match (Str.regexp {|.*\(ab\|cd\|pq\|xy\).*|}) s 0 |> not
-    && Str.string_match
-         (Str.regexp
+    && Re.Str.string_match (Re.Str.regexp {|.*\(ab\|cd\|pq\|xy\).*|}) s 0 |> not
+    && Re.Str.string_match
+         (Re.Str.regexp
             {|.*\(aa\|bb\|cc\|dd\|ee\|ff\|gg\|hh\|ii\|jj\|kk\|ll\|mm\|nn\|oo\|pp\|qq\|rr\|ss\|tt\|uu\|vv\|ww\|xx\|yy\|zz\).*|})
          s 0
 
@@ -29,8 +29,8 @@ module P2 = struct
   let is_fine s =
     let sList = Lib.string_2_char_list s in
     loop_on_list sList
-    && Str.string_match
-         (Str.regexp
+    && Re.Str.string_match
+         (Re.Str.regexp
             {|.*\(a.a\|b.b\|c.c\|d.d\|e.e\|f.f\|g.g\|h.h\|i.i\|j.j\|k.k\|l.l\|m.m\|n.n\|o.o\|p.p\|q.q\|r.r\|s.s\|t.t\|u.u\|v.v\|w.w\|x.x\|y.y\|z.z\).*|})
          s 0
 
--- bin/y2015/src/day12.ml
+++ bin/y2015/src/day12.ml
@@ -1,3 +1,34 @@
-let cnt = Lib.read_file "15" "12" Lib.id
-let part1 () = ()
-let part2 () = ()
+let cnt = Lib.read_file "15" "12" Lib.id |> List.hd
+
+module P1 = struct
+  let parse_line line =
+    let rec aux pos =
+      try
+        let sub_str_pos =
+          Re.Str.search_forward (Re.Str.regexp {|-?[0-9]+|}) line pos
+        in
+        let sub_str = Re.Str.matched_string line in
+        int_of_string sub_str + aux (sub_str_pos + String.length sub_str)
+      with Not_found -> 0
+    in
+    aux 0
+end
+
+module P2 = struct
+  let remove_red cnt =
+    let cnt_parsed = Yojson.Basic.from_string cnt in
+    let rec aux (ct : Yojson.Basic.t) =
+      match ct with
+      | `List l -> l |> List.fold_left (fun acc e -> acc + aux e) 0
+      | `Assoc k ->
+          if List.exists (fun (_, b) -> b = `String "red") k then 0
+          else List.fold_left (fun acc e -> acc + aux e) 0 (List.map snd k)
+      | `String _ -> 0
+      | `Int i -> i
+      | _ -> raise Lib.Invalid_input
+    in
+    aux cnt_parsed
+end
+
+let part1 () = P1.parse_line cnt |> print_int
+let part2 () = P2.remove_red cnt |> print_int
