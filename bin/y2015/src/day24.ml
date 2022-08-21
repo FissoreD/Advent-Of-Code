b22efb9cd42ed522b3a158b96a436e674eacf777
FissoreD <davide.fissore@etu.univ-cotedazur.fr>
2022-08-21 11:55:58 +0200

    Update day24.ml

--- bin/y2015/src/day24.ml
+++ bin/y2015/src/day24.ml
@@ -6,37 +6,32 @@ let cnt =
 
 let total = List.fold_left ( + ) 0 cnt
 
-module P1 = struct
-  let rec sub_list tot group_weight = function
-    | [] -> []
-    | hd :: tl ->
-        if tot + List.fold_left ( + ) 0 tl + hd < group_weight then []
-        else (hd :: tl) :: sub_list tot group_weight tl
+let rec sub_list tot group_weight = function
+  | [] -> []
+  | _ :: tl as l -> l :: sub_list tot group_weight tl
 
-  let main group_weight =
-    let min_len = ref max_int in
-    let res : int list ref = ref [ max_int ] in
-    let rec find_groups2 ?(acc = []) ?(tot = 0) l =
-      if not (l = [] || tot > group_weight || List.length acc > !min_len) then
-        List.iter
-          (fun l ->
-            let acc, tot, tl =
-              match l with
-              | [] -> (acc, tot, [])
-              | hd :: tl -> (hd :: acc, tot + hd, tl)
-            in
-            if tot = group_weight && List.length acc <= !min_len then (
-              min_len := List.length acc;
-              if List.length !res = !min_len then (
-                if List.fold_left ( * ) 1 !res > List.fold_left ( * ) 1 acc then
-                  res := acc)
-              else res := acc);
-            find_groups2 ~acc ~tot tl)
-          (sub_list tot group_weight l)
-    in
-    find_groups2 cnt;
-    List.fold_left ( * ) 1 !res
-end
+let smallest_product a b = List.fold_left ( * ) 1 a > List.fold_left ( * ) 1 b
 
-let part1 () = P1.main (total / 3) |> string_of_int |> print_endline
-let part2 () = P1.main (total / 4) |> string_of_int |> print_endline
+let update_acc_tot acc tot l =
+  if l = [] then (acc, tot, []) else List.(hd l :: acc, tot + hd l, tl l)
+
+let main group_weight =
+  let min_len, res = (ref max_int, ref [ max_int ]) in
+  let rec find_groups2 ?(acc = []) ?(tot = 0) l =
+    if not (l = [] || tot > group_weight || List.length acc > !min_len) then
+      List.iter
+        (fun l ->
+          let acc, tot, tl = update_acc_tot acc tot l in
+          if tot = group_weight && List.length acc <= !min_len then (
+            min_len := List.length acc;
+            if List.length !res = !min_len then (
+              if smallest_product !res acc then res := acc)
+            else res := acc);
+          find_groups2 ~acc ~tot tl)
+        (sub_list tot group_weight l)
+  in
+  find_groups2 cnt;
+  List.fold_left ( * ) 1 !res
+
+let part1 () = main (total / 3) |> string_of_int |> print_endline
+let part2 () = main (total / 4) |> string_of_int |> print_endline
