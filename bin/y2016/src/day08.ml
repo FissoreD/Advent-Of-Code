47a1c38f911ea2b54f4c306872a29f6d1769b1ff
FissoreD <davide.fissore@etu.univ-cotedazur.fr>
2022-08-22 21:40:31 +0200

    Update day08.ml

--- bin/y2016/src/day08.ml
+++ bin/y2016/src/day08.ml
@@ -8,20 +8,18 @@ type instr =
   | RotateY of rotate
 
 let cnt =
+  let open String in
   let open Lib.List in
   let funct line =
-    let s = String.split_on_char ' ' line in
+    let s = split_on_char ' ' line in
     let get_rotate s =
-      {
-        n = String.split_on_char '=' (nth s 2) |> get 1 |> int_of_string;
-        steps = nth s 4 |> int_of_string;
-      }
+      let n = split_on_char '=' (nth s 2) |> get 1 |> int_of_string in
+      { n; steps = nth s 4 |> int_of_string }
     in
-    if String.starts_with ~prefix:"rect" line then
-      let s = nth s 1 |> String.split_on_char 'x' |> map int_of_string in
+    if starts_with ~prefix:"rect" line then
+      let s = nth s 1 |> split_on_char 'x' |> map int_of_string in
       Rect { w = hd s; h = nth s 1 }
-    else if String.starts_with ~prefix:"rotate row" line then
-      RotateY (get_rotate s)
+    else if starts_with ~prefix:"rotate row" line then RotateY (get_rotate s)
     else RotateX (get_rotate s)
   in
 
@@ -56,20 +54,19 @@ module P1 = struct
           arr.(n).(0) <- last
         done
 
-  let update_array arr = List.iter (apply_inst arr) cnt
+  let update_array arr =
+    List.iter (apply_inst arr) cnt;
+    arr
 
   let main () =
-    let arr = make_screen () in
-    (* List.iter (apply_inst arr) cnt; *)
-    update_array arr;
+    let arr = update_array (make_screen ()) in
     Array.map (Array.map (function '#' -> 1 | _ -> 0)) arr
     |> Array.fold_left (Array.fold_left ( + )) 0
 end
 
 module P2 = struct
   let main () =
-    let arr = make_screen () in
-    P1.update_array arr;
+    let arr = P1.update_array @@ make_screen () in
     for j = 0 to Array.length arr.(0) - 1 do
       for i = 0 to Array.length arr - 1 do
         print_char arr.(i).(j)
